e07c85500659d6aa737a2f5cd60e07c7f72161cd
Rashmi Banthia <rjain29@gmail.com>
2020-09-09 21:38:42 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='AC297r Capstone',
+    description='AC295',
     author='Harvard IACS',
     license='',
 )
