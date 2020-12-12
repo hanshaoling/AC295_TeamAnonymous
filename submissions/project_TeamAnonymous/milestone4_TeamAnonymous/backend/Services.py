b2655a25da2ca63265afe61116727250f8ef6531
zdd <zdd@dhcp-10-250-172-45.harvard.edu>
2020-12-12 17:56:18 -0500

    change to gru model

--- submissions/project_TeamAnonymous/milestone4_TeamAnonymous/backend/Services.py
+++ submissions/project_TeamAnonymous/milestone4_TeamAnonymous/backend/Services.py
@@ -6,11 +6,10 @@ from tensorflow.keras.models import Model,Sequential
 from tensorflow.keras.models import model_from_json
 from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
 from tensorflow.keras.layers import LSTM, Embedding, Dense, GRU, Input
-from transformers import BertTokenizer, TFBertForSequenceClassification, TFBertModel
 from tensorflow.keras import layers
 
 
-def build_model():
+def build_bert_model():
    '''
 
    :return: Bert model structure
@@ -33,13 +32,17 @@ def build_model():
    inputs = {'input_ids': input_ids, 'token_type_ids': token_type_ids, 'attention_mask': attention_mask}
    bert_model = keras.Model(inputs= inputs, outputs= output,name='BERT_Model')
    return bert_model
+def build_model(model_path='glo_gru'):
+    model=tf.keras.models.load_model('glo_gru')
+    return model
 
 def get_essay_score(essay_prompt, essay):
     # essay: a string
     # essay_prompt: int
     model=build_model()
     #load bert weight
-    model.load_weights("BERT_Model.h5")
+    #model.load_weights("BERT_Model.h5")
+    '''
     #### missing tokenizer
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased',do_lower_case=True)
     input_x = tokenizer.encode_plus(essay,
@@ -50,9 +53,10 @@ def get_essay_score(essay_prompt, essay):
         truncation='longest_first',
         return_tensors="tf"
     )
+    '''
     #print(input_x)
 
-    score = model.predict((input_x['input_ids'], input_x['token_type_ids'], input_x['attention_mask']))[0][0]
+    score = model.predict([essay])[0][0]
     #input_x= tokenizer(...)
     #score=model.predict(input_x))
     low_scale={1:2,2:1,3:0,4:0,5:0,6:0,7:0,8:0}
@@ -60,18 +64,3 @@ def get_essay_score(essay_prompt, essay):
     score=score*(high_scale[essay_prompt]-low_scale[essay_prompt])+low_scale[essay_prompt]
     score=int(score.round(0))
     return score
-
-# gap_img_path = 'gap_images/gap_images/'
-# img_shape = (32,32)
-# img_list = glob.glob(gap_img_path + '*.jpg')
-# feature_map = {}
-# weights = 'ae_weights.h5'; json_file_path = 'autoencoder.json'
-# ae = load_model(json_file_path, weights)
-# for img in tqdm(img_list):
-#     img_ae = prepare_ae_input(img)
-#     img_feature = get_feature_maps(ae, img_ae)
-#     feature_map[img] = img_feature.tolist()
-
-# with open('feature_map.json', 'w') as fp:
-#     json.dump(feature_map, fp)
-# print("Dump complete")s
--- submissions/project_TeamAnonymous/milestone4_TeamAnonymous/backend/backend_new.py
+++ submissions/project_TeamAnonymous/milestone4_TeamAnonymous/backend/backend_new.py
@@ -5,10 +5,8 @@ import pandas as pd
 import numpy as np
 import requests
 import tensorflow as tf
-from Services import get_essay_score
 app = Flask(__name__)
 api = Api(app)
-gap_img_path = 'gap_images/gap_images/'
 
 essay_prompt_dict={"Effect of Computer on people": 1,
                    "Censorship in the libraries" : 2,
@@ -18,6 +16,36 @@ essay_prompt_dict={"Effect of Computer on people": 1,
                    "Source dependant essay: The Mooring Mast" : 6,
                    "Patience" : 7,
                    "The benefits of laughter" : 8}
+
+model = tf.keras.models.load_model('glo_gru')
+def get_essay_score(essay_prompt, essay,model=model):
+    # essay: a string
+    # essay_prompt: int
+    #load bert weight
+    #model.load_weights("BERT_Model.h5")
+    '''
+    #### missing tokenizer
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased',do_lower_case=True)
+    input_x = tokenizer.encode_plus(essay,
+        add_special_tokens = True, # add [CLS], [SEP]
+        max_length = 512, # max length of the text that can go to BERT (<=512)
+        padding='max_length',
+        return_attention_mask = True, # add attention mask to not focus on pad tokens
+        truncation='longest_first',
+        return_tensors="tf"
+    )
+    '''
+    #print(input_x)
+
+    score = model.predict([essay])[0][0]
+    #input_x= tokenizer(...)
+    #score=model.predict(input_x))
+    low_scale={1:2,2:1,3:0,4:0,5:0,6:0,7:0,8:0}
+    high_scale={1:12,2:6,3:3,4:3,5:4,6:4,7:30,8:60}
+    score=score*(high_scale[essay_prompt]-low_scale[essay_prompt])+low_scale[essay_prompt]
+    score=int(score.round(0))
+    return score
+
 class EssayScore(Resource):
     def get(self):
         
